6f4b0c618afe24b3a53e0564eee3a7f75bc81721
Ranee Mikaella Gutierrez <raneemikaellavgutierrez@gmail.com>
2026-09-10 08:02:53 +0800

    Add authentication and cloud project controls to editor shell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { AuthProjectControls } from "@/components/auth/auth-project-controls";
 import { MotionPlayground } from "@/components/editor/motion-playground";
 
 export default function Home() {
-  return <MotionPlayground />;
+  return (
+    <div className="relative">
+      <MotionPlayground />
+      <div className="fixed left-1/2 top-3 z-[90] -translate-x-1/2">
+        <AuthProjectControls />
+      </div>
+    </div>
+  );
 }
