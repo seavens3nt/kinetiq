12101d6e5d0dfbf68a6b871a9cac885132bd0a05
Ranee Mikaella Gutierrez <raneemikaellavgutierrez@gmail.com>
2026-09-14 03:08:28 +0800

    Fix auth controls positioning and modal containing block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { MotionPlayground } from "@/components/editor/motion-playground";
 
 export default function Home() {
   return (
-    <div className="relative">
+    <div className="relative min-h-screen bg-[#09090c]">
       <MotionPlayground />
-      <div className="fixed left-1/2 top-3 z-[90] -translate-x-1/2">
+      <div className="fixed right-5 top-3 z-[90]">
         <AuthProjectControls />
       </div>
     </div>
