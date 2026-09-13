8f66516c45409ca81e7c86b2faeb0959df5f8fb4
Ranee Mikaella Gutierrez <raneemikaellavgutierrez@gmail.com>
2026-09-14 05:30:51 +0800

    Theme Kinetiq studio shell

--- src/app/studio/page.tsx
+++ src/app/studio/page.tsx
@@ -3,7 +3,7 @@ import { MotionPlayground } from "@/components/editor/motion-playground";
 
 export default function StudioPage() {
   return (
-    <div className="relative min-h-screen bg-[#09090c]">
+    <div className="kinetiq-studio-shell relative min-h-screen bg-[#09090c]">
       <MotionPlayground />
       <div className="fixed right-4 top-2.5 z-[90] sm:right-5">
         <AuthProjectControls />
