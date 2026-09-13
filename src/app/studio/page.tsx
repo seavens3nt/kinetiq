ac8229f985628cef316ea9fe2590b88ef3e1786d
Ranee Mikaella Gutierrez <raneemikaellavgutierrez@gmail.com>
2026-09-14 03:34:51 +0800

    Move Kinetiq editor to studio route

---
+++ src/app/studio/page.tsx
@@ -0,0 +1,13 @@
+import { AuthProjectControls } from "@/components/auth/auth-project-controls";
+import { MotionPlayground } from "@/components/editor/motion-playground";
+
+export default function StudioPage() {
+  return (
+    <div className="relative min-h-screen bg-[#09090c]">
+      <MotionPlayground />
+      <div className="fixed right-4 top-2.5 z-[90] sm:right-5">
+        <AuthProjectControls />
+      </div>
+    </div>
+  );
+}
